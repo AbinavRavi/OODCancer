118abfebbb2f188fb1e1129fa407b0686fd44d8d
AbinavRavi <abinav.ravi@tum.de>
2020-01-18 11:52:22 +0100

    added median frequency for training

--- model/loss.py
+++ model/loss.py
@@ -1,5 +1,11 @@
 import torch.nn as nn
-loss = nn.CrossEntropyLoss()
+from config.data_utils import class_weights
+
+if(cuda.is_available == True):
+    class_weights.cuda()
+else:
+    pass
+loss = nn.CrossEntropyLoss(weight=class_weights)
 def loss_fn(input,label):
     losses = loss(input,label)
     return losses
